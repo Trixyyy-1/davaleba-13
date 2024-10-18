970ec28ed05a5651323720ae3634c8989e3c3f98
Vakho Lezhava <xxunxul@gmail.com>
2024-10-18 09:02:27 -0600

    script
    idk

---
+++ script.js
@@ -0,0 +1,24 @@
+// cvladebi
+let Name = "vaxo";                  
+let Lastname = "lezhava";              
+let Age = 19;                        
+let Email = "xxunxul@gmail.com";      
+let Number = "599280343";   
+let Adult = true;     
+
+// asakis ganaxleba
+function New_Age (New_Age) {
+    Age = New_Age;
+}
+
+// console
+console.log("Name:", Name);
+console.log("Lastname:", Lastname);
+console.log("Age:", Age);
+console.log("Email:", Email);
+
+// Boolean 
+console.log("%c Adult: " + Adult, "color: yellow;");
+
+// Number
+console.log("%c Number: " + Number, "color: red;");
